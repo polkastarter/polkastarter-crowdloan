b07459dc42baf0a27ecfe3f567bf94c3867d31bb
ppoliani <ppoliani@gmail.com>
2021-02-28 11:15:44 +0000

    feat(*): configure the sidebar

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -80,7 +80,7 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"Creative Tim"}
+        logoText={"Polkastarter"}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
