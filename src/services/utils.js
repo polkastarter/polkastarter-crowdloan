f4b1f5807b764a5e63749c62047cdd14583069cd
ppoliani <ppoliani@gmail.com>
2021-03-12 08:56:52 +0000

    fix(*): remove random variable

--- src/services/utils.js
+++ src/services/utils.js
@@ -4,4 +4,3 @@ export const bn = num => new BN(num)
 export const toNumber = substrateNum => substrateNum.toNumber()
 export const fromPlunk = num => bn(num).div(bn(10).pow(bn(12)))
 export const toPlunk = num => bn(num).mul(bn(10).pow(bn(12)))
-polk
