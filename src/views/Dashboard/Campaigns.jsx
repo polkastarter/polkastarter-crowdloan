1484e02a39774befa247a2790e4898fbb09824d2
ppoliani <ppoliani@gmail.com>
2021-04-02 14:52:24 +0100

    chore(*): update the active campaign index

--- src/views/Dashboard/Campaigns.jsx
+++ src/views/Dashboard/Campaigns.jsx
@@ -2,11 +2,11 @@ import React from "react"
 import Campaign from './Campaign'
 
 const Campaigns = props => {
-  const {fundCount=0} = props
+  const {fundCount=1} = props
 
   return (
     new Array(fundCount).fill().map((_, i) => (
-      <Campaign key={i} fundIndex={i} />
+      <Campaign key={i} fundIndex={155} />
     ))
   )
 }
